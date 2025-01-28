f2105f615d0fcca7685f520182de71b902572351
Fergenson <fergenson.beats@yandex.ru>
2025-01-28 21:24:42 +0300

    Что-то поломалось с добавлением scrollView. Изображение не размещается по центру, хотя сделано все, как в авторском решении. Сначала зум работал, потом почему то перестал (в эмуляторе не работает зумирование).

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -4,12 +4,16 @@ final class SingleImageViewController:UIViewController {
     
     var image: UIImage? {
             didSet {
-                guard isViewLoaded else { return }
+                guard isViewLoaded, let image else { return }
                 imageView.image = image
+                imageView.frame.size = image.size
             }
         }
     
     @IBOutlet private var imageView: UIImageView!
+    
+    @IBOutlet private weak var scrollView: UIScrollView!
+    
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
@@ -17,6 +21,15 @@ final class SingleImageViewController:UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+    }
+    
+}
+
+extension SingleImageViewController:UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
     
 }
